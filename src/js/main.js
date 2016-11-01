1d4df2279f4dd1f31ad4ee25d9ee9ccc2abb3cf6
johnmberger <johnmartinberger@gmail.com>
2016-11-01 11:36:23 -0600

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// jquery test
+$(document).on('ready', () => {
+  console.log('sanity check!');
+});
+
+// es6 test
+const PI = 3.14;
+
+// mocha test
+function sum(num1, num2) {
+  return parseInt(num1) + parseInt(num2);
+}
